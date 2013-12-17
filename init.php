28c62d5541f0848e8ec03808bca0d3a9c07c7de4
Alexander Standfest <standfest@teleglobal.lv>
2013-12-17 21:57:00 +0200

    added Flysystem loading

--- init.php
+++ init.php
@@ -1,2 +1,13 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
+spl_autoload_register(function ($class) {
+    if (!substr($class, 0, 9) === 'Flysystem') {
+        return;
+    }
+
+    $location = __DIR__.'/vendor/flysystem/src/'.str_replace('\\', '/', $class).'.php';
+
+    if (is_file($location)) {
+        require_once($location);
+    }
+});
